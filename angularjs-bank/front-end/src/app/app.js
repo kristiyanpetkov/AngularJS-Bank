5a8dc44b1ca10a17fe3fcca8892a022ef98a0e2a
kristiyanpetkov <kristiqn.l.petkov@gmail.com>
2016-08-17 17:16:59 +0300

    bank: added skeleton of the project
    Change-Id: Ief736552f951af8a469e8f4cd2adf5ec510274e7

---
+++ angularjs-bank/front-end/src/app/app.js
@@ -0,0 +1,22 @@
+angular.module('bank', [
+    'templates-app',
+    'templates-common',
+    'bank.home',
+    'ui.router'
+])
+
+    .config(function myAppConfig($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/home');
+    })
+
+
+    .controller('AppCtrl', function AppCtrl($scope, $location) {
+        $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+            if (angular.isDefined(toState.data.pageTitle)) {
+                $scope.pageTitle = toState.data.pageTitle + ' | bank';
+            }
+        });
+    })
+
+;
+
---
+++ angularjs-bank/front-end/src/app/home/home.js
@@ -0,0 +1,24 @@
+
+angular.module( 'bank.home', [
+  'ui.router'
+])
+
+.config(function config( $stateProvider ) {
+  $stateProvider.state( 'home', {
+    url: '/home',
+    views: {
+      "main": {
+        controller: 'HomeCtrl',
+        templateUrl: 'home/home.tpl.html'
+      }
+    },
+    data:{ pageTitle: 'Bank Home Page' }
+  });
+})
+
+
+.controller( 'HomeCtrl', function HomeController( $scope ) {
+})
+
+;
+
